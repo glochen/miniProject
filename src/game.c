75a88e1dd9bdb6936f1e63fa27b1f15b6a230271
Alex Han <hexalanshole@gmail.com>
2019-04-16 22:49:15 -0400

    Added more functions to pegs.h. Implemented updateSlots().

--- src/game.c
+++ src/game.c
@@ -4,6 +4,40 @@
 Slot slots[NUM_SLOTS];
 int ActiveSlot; // the currently selected slot
 
+
+int min(int x, int y){
+    if (x < y){
+        return x;
+    }
+    else{
+        return y;
+    }
+}
+
+void descend(int slot){
+    if (slots[slot].state > 0){
+        slots[slot].state = Open;
+    }
+}
+
+void elevate(int slot){
+    if (slots[slot].state > 0){
+        slots[slot].state = Legal;
+    }
+}
+
+void updateSlots(){
+    for(int i = 0; i < NUM_SLOTS; i++){
+        // Check the neighbor status of each slot
+        if(slots[ActiveSlot].neighbours[i] < 0){
+            descend(i);
+        }
+        else{
+            elevate(i);
+        }
+    }
+}
+
 bool jump(int source, int dest){
     // If slot numbers are not in the correct range, return failure
     if (source < 0 || dest < 0 || source >= NUM_SLOTS || dest >= NUM_SLOTS){
